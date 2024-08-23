238e40db9ab12df850b94274958bfe7dba3a1c57
unknown <sonajithin34@gmail.com>
2024-08-23 15:18:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import Signup from './components/Signup'
+
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+  
+     <Signup/>
+     
+  
+    </>
+  )
+}
+
+export default App
